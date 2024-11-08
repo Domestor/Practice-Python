d3482db853621b555b948f49c039271c09d160ab
Vetal1001 <Vetal201710@gmail.com>
2024-11-08 19:45:24 +0200

    unnecessary files have been deleted

--- palindrome.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def is_palindrome(self, x: int) -> bool:
-        str_x = str(x)
-        return str_x == str_x[::-1]
-
-sol = Solution()
-
-print(sol.is_palindrome(121))
-print(sol.is_palindrome(10))
